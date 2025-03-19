0807cb36680261da3f9db7c12a0ffb7b9bd6c046
SirSaito <89697576+SirSaito@users.noreply.github.com>
2025-03-19 14:45:46 -0300

    Add files via upload

---
+++ 19_03_2025_ex_01_v2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <malloc.h>
+
+//Escreva um programa para declarar, utilizando ALOCAÇÃO DINÂMICA DE
+//MEMÓRIA, um vetor com 10 elementos DO TIPO INTEIRO, em C OU C++.
+//Atribua os seguintes valores aos elementos do vetor:
+//2 : 5 : 8 : 11 : 14 : 17 : 20 : 23 : 26 : 29
+//Imprima os valores do vetor.
+//Libere a área de memória utilizada pelo vetor.
+
+int main(){
+
+   int i, j;
+   //int v[10]={2, 5, 8, 11, 14, 17, 20, 23, 26, 29};
+   int *x;
+
+   x = (int *) malloc (10 *sizeof(int));
+
+   if (x == NULL)
+
+       printf("\t\tERRO de alocacao de memoria\n");
+
+   else{
+       for(i=0,j=2; i < 10; i++, j+=3){
+           x[i]=j;
+       }
+       for(i=0; i < 10; i++){
+           printf("%d\n", x[i]);
+       }
+       free(x);
+   }
+
+   return 0;
+
+}
